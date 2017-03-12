2146bb44968b7c8f069d055c5182e744695927a6
Thomas Loubiou <t.loubiou@systonic.fr>
2017-03-12 18:19:54 +0100

    Disable anti-aliasing

--- src/game.ts
+++ src/game.ts
@@ -5,7 +5,7 @@ import { GameState } from "./game.state";
 
 export class Game extends Phaser.Game {
     constructor() {
-        super(800, 600, Phaser.AUTO);
+        super(800, 600, Phaser.AUTO, "", null, false, false);
         this.state.add("boot", Boot);
         this.state.add("start", Start);
         this.state.add("mainMenu", MainMenu);
