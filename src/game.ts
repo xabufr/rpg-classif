263bfd035e072f97b68b359f5446f01ccd118ab2
Thomas Loubiou <t.loubiou@systonic.fr>
2017-03-12 18:58:35 +0100

    Balck background

--- src/game.ts
+++ src/game.ts
@@ -5,7 +5,7 @@ import { GameState } from "./game.state";
 
 export class Game extends Phaser.Game {
     constructor() {
-        super(800, 600, Phaser.AUTO, "", null, false, false);
+        super(800, 600, Phaser.AUTO, "game", null, false, false);
         this.state.add("boot", Boot);
         this.state.add("start", Start);
         this.state.add("mainMenu", MainMenu);
