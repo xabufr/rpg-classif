abfffa5f5d4c06685a435822bb535dfbfa8f9a92
Kalavecy <cass.kalamur@gmail.com>
2017-04-11 23:47:59 +0200

    Some changes on pie charts

--- public/script/pie-stats.js
+++ public/script/pie-stats.js
@@ -1,7 +1,11 @@
-var TOTAL_VOTE = 38,                              
-    RESULTATS = [31, 7, 20, 14, 4],
-    BACKGROUND = ["rgba(194, 61, 255, 1)","rgba(69,224,211, 1)","#FFCE56"];
+// Variables utilisées pour TOUS les graph (dans l'ordre)
 
+var TOTAL_VOTE = 39,                              
+    RESULTATS = [32, 7, 0, 21, 14, 4, 37, 2],
+    BACKGROUND = ["#3366cc","#dc3912","#ff9900","#109618"],
+    BACKGROUND_FULL = [BACKGROUND[0], BACKGROUND[1], BACKGROUND[2], BACKGROUND[3]];
+
+// Pie 1
 var canvas1 = document.getElementById("pieChart1");
 var ctx1 = document.getElementById("pieChart1").getContext("2d");
 var pieChart1 = parseInt(new Chart(ctx1, {
@@ -14,8 +18,8 @@ var pieChart1 = parseInt(new Chart(ctx1, {
                     (Math.floor((RESULTATS[1]/TOTAL_VOTE)*1000))/10
                     ],
                 // Pourquoi ne pas utiliser une boucle de style (Math.floor((RESULTATS[i]/TOTAL_VOTE)*1000))/10 avec i++ ?
-                backgroundColor: [BACKGROUND[0], BACKGROUND[1]],
-                hoverBackgroundColor: [BACKGROUND[0], BACKGROUND[1]],
+                backgroundColor: BACKGROUND_FULL,
+                hoverBackgroundColor: BACKGROUND_FULL,
             }],
         labels: [
             "Oui (" + (Math.floor((RESULTATS[0]/TOTAL_VOTE)*1000))/10 + "%)", 
@@ -25,7 +29,7 @@ var pieChart1 = parseInt(new Chart(ctx1, {
     options: {
         title: {
             display: true,
-            text: 'L\'école doit-elle se numériser d\'avantage ?',
+            text: 'Pensez-vous que l\'école doit se numériser d\'avantage ?',
         },
         animation: {
             responsive: false,
@@ -35,31 +39,35 @@ var pieChart1 = parseInt(new Chart(ctx1, {
     }
 }));
 
+
+// Pie 2
 var canvas2 = document.getElementById("pieChart2");
 var ctx2 = document.getElementById("pieChart2").getContext("2d");
 var pieChart2 = new Chart(ctx2, {
     type: 'pie',
     data: {
         labels: [
-            "Intéressante (" + (Math.floor((RESULTATS[2]/TOTAL_VOTE)*1000))/10 + "%)", 
-            "Pertinente (" + (Math.floor((RESULTATS[3]/TOTAL_VOTE)*1000))/10 + "%)", 
-            "Les deux (" + (Math.floor((RESULTATS[4]/TOTAL_VOTE)*1000))/10 + "%)"
+            "Ni l'un, ni l'autre (" + (Math.floor((RESULTATS[2]/TOTAL_VOTE)*1000))/10 + "%)",
+            "Intéressante (" + (Math.floor((RESULTATS[3]/TOTAL_VOTE)*1000))/10 + "%)", 
+            "Pertinente (" + (Math.floor((RESULTATS[4]/TOTAL_VOTE)*1000))/10 + "%)", 
+            "Les deux (" + (Math.floor((RESULTATS[5]/TOTAL_VOTE)*1000))/10 + "%)"
             ],
         datasets: [
             {
                 data: [
                     (Math.floor((RESULTATS[2]/TOTAL_VOTE)*1000))/10, 
                     (Math.floor((RESULTATS[3]/TOTAL_VOTE)*1000))/10, 
-                    (Math.floor((RESULTATS[4]/TOTAL_VOTE)*1000))/10
+                    (Math.floor((RESULTATS[4]/TOTAL_VOTE)*1000))/10,
+                    (Math.floor((RESULTATS[5]/TOTAL_VOTE)*1000))/10
                     ],
-                backgroundColor: ["rgba(194, 61, 255, 1)","#36A2EB","#FFCE56"],
-                hoverBackgroundColor: ["rgba(194, 61, 255, 1)","#36A2EB","#FFCE56"],
+                    backgroundColor: BACKGROUND_FULL,
+                    hoverBackgroundColor: BACKGROUND_FULL,
             }]
     },
     options: {
         title: {
             display: true,
-            text: 'Trouvez-vous l\'idée de départ :',
+            text: 'Vous avez trouvé l\'idée de départ :',
         },
         animation: {
             responsive: false,
@@ -68,3 +76,37 @@ var pieChart2 = new Chart(ctx2, {
         }
     }
 });
+
+// Pie 3
+var canvas3 = document.getElementById("pieChart3");
+var ctx3 = document.getElementById("pieChart3").getContext("2d");
+var pieChart3 = parseInt(new Chart(ctx3, {
+    type: 'pie',
+    data: {
+        datasets: [
+            {
+                data: [
+                    (Math.floor((RESULTATS[6]/TOTAL_VOTE)*1000))/10, 
+                    (Math.floor((RESULTATS[7]/TOTAL_VOTE)*1000))/10
+                    ],
+                // Pourquoi ne pas utiliser une boucle de style (Math.floor((RESULTATS[i]/TOTAL_VOTE)*1000))/10 avec i++ ?
+                backgroundColor: BACKGROUND_FULL,
+                hoverBackgroundColor: BACKGROUND_FULL,
+            }],
+        labels: [
+            "Oui (" + (Math.floor((RESULTATS[6]/TOTAL_VOTE)*1000))/10 + "%)", 
+            "Non (" + (Math.floor((RESULTATS[7]/TOTAL_VOTE)*1000))/10 + "%)"
+            ],       
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Peut-on véhiculer des savoirs par le biais de jeu vidéo ?',
+        },
+        animation: {
+            responsive: false,
+            maintainRatio: true,
+            animateScale:true,
+        }
+    }
+}));
